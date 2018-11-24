14038913cecbe737c9d5538ccbf590ab534d6a63
xingruping <xingruping@163.com>
2018-11-24 19:55:38 +0800

    添加一个test工程到git

---
+++ git_github/src/main/java/com/xingruping/Test.java
@@ -0,0 +1,10 @@
+package com.xingruping;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbb");
+        System.out.println("cccccccccccccccc");
+        System.out.println("dddddddddddddddd");
+    }
+}
